5e5e233058debacdbe0abf5933504dd3f53c6fd8
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-11-12 23:07:12 +0100

    Modified method compare in SortByDate and successed all of the test

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,15 +1,14 @@
 package it.unibo.nestedenum;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
-import java.lang.String;
+import java.util.Locale;
+import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
-    
+
     @Override
     public Comparator<String> sortByDays() {
         return new SortByDate();
@@ -21,60 +20,55 @@ public final class MonthSorterNested implements MonthSorter {
     }
 
     private enum Month{
-        JANUARY("JANUARY", 31),
-        FEBRUARY("FEBRUARY", 28),
-        MARCH("MARCH", 31),
-        APRIL("APRIL", 30),
-        MAY("MAY", 31),
-        JUNE("JUNE", 30),
-        JULY("JULY", 31),
-        AUGUST("AUGUST", 31),
-        SEPTEMBER("SEPTEMBER", 30),
-        OCTOBER("OCTOBER", 31),
-        NOVEMBER("NOVEMBER", 30),
-        DICEMBER("DICEMBER", 31);
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
 
         private final int daysOfTheMonth;
-        private final String nameOfTheMonth;
 
-        private Month(final String name, final int days){
+        Month(final int days){
             this.daysOfTheMonth = days;
-            this.nameOfTheMonth = name;
         }
 
         public static Month fromString(final String stringedMonth) {
+            Objects.requireNonNull(stringedMonth);
             Month found = null;
-            List<Month> occurencyList = new ArrayList<>();
-            for(Month month : values()){
-                if(month.nameOfTheMonth.startsWith(stringedMonth.toUpperCase())){
-                    occurencyList.add(month);
+            int occurrency = 0;
+            for(final Month month : Month.values()){
+                if(month.toString().toLowerCase(Locale.ROOT).startsWith(stringedMonth.toLowerCase(Locale.ROOT))){
+                    occurrency++;
+                    found = month;
                 }
             }
-            if(occurencyList.size() > 1){
+            if(occurrency > 1){
                 throw new IllegalArgumentException("Ambiguous string");
             }
-            else if(occurencyList.size() == 1){
-                found = occurencyList.get(0);
-            }
-            else if(occurencyList.isEmpty()){
+            if(found == null || occurrency == 0){
                 throw new IllegalArgumentException("The given string is not a month");
             }
             return found;
         }
     }
     
-    private static class SortByDate implements Comparator<String>{
+    private class SortByDate implements Comparator<String>{
 
         @Override
         public int compare(final String o1, final String o2) {
-            final Month firstMonth = Month.fromString(o1);
-            final Month secondMonth = Month.fromString(o2);
-            return Integer.compare(secondMonth.daysOfTheMonth, firstMonth.daysOfTheMonth);
+            return Month.fromString(o1).daysOfTheMonth - Month.fromString(o2).daysOfTheMonth;
         }
 
     }
 
-    private static class SortByMonthOrder implements Comparator<String>{
+    private class SortByMonthOrder implements Comparator<String>{
 
         @Override
         public int compare(final String o1, final String o2) {
